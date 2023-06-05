0d3b69d61b7d1f1dbc0ff58e81bfddfb7e456c49
manasa maddipatla <manasamaddipatla@gmail.com>
2023-06-05 16:06:11 +0530

    first commit

---
+++ app/forms.py
@@ -0,0 +1,22 @@
+from app.models import *
+from django import forms
+
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model=User
+        fields=['username','password']
+        widgets={'password':forms.PasswordInput}
+
+class QuestionForm(forms.ModelForm):
+    class Meta:
+        model=Question
+        fields=['title','description']
+        widgets={'description':forms.TextInput(attrs={'class':'form-control'})}
+
+
+class AnswerForm(forms.ModelForm):
+    class Meta:
+        model=Answer
+        fields=['question','description']
+        widgets={'address':forms.TextInput(attrs={'class':'form-control'})}
